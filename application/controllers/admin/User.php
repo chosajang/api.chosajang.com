090d5c56c37505e0f0a2ca480e452ed6dbb1e3fc
ChoSaJang <enjoy.hyun@gmail.com>
2020-08-24 20:09:38 +0900

    - 크로스 도메인 사용관련

--- application/controllers/admin/User.php
+++ application/controllers/admin/User.php
@@ -9,9 +9,6 @@ class User extends CI_Controller {
     public $callback;
 
     public function __construct() {
-        // 크로스 도메인 사용관련
-        header_cors();
-
         parent::__construct();
         $this->method_prefix = '_user_';
         
@@ -27,6 +24,9 @@ class User extends CI_Controller {
     public function _remap($function) {
         if ( $function == 'index' || $function == '' ) { $function = ''; }
         $method = $this->method_prefix . $function;
+
+        // 크로스 도메인 사용관련
+        header_cors();
 
         /**
          * view로 전달할 공통 정보
