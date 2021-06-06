cea61686ae86fb01111531ba9d16fdb2cb1193ff
jonny <enjoy.hyun@gmail.com>
2021-06-06 22:40:16 +0900

    유저모델 추가 후, 인증 클래스 설정

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\tb_user::class,
         ],
 
         // 'users' => [
