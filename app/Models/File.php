6acbcac1900daa6b4cd4652c24cc2ad23fb0c192
hyun <enjoy.hyun@gmail.com>
2021-07-02 11:01:59 +0900

    어노테이션 추가

--- app/Models/File.php
+++ app/Models/File.php
@@ -5,6 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $file_seq
+ * @property string $logical_name
+ * @property string $physical_name
+ * @property string $path
+ * @property float $size
+ * @property string $mimetype
+ * @property string $use_yn
+ * @property Carbon $created_at
+ * 
+ * @package App\Models
+ */
 class File extends Model
 {
     use HasFactory;
@@ -12,12 +24,28 @@ class File extends Model
     protected $table = 'tb_file';
     protected $primaryKey = 'file_seq';
 
+    const FILE_SEQ = 'file_seq';
+    const LOGICAL_NAME = 'logical_name';
+    const PHYSICAL_NAME = 'physical_name';
+    const PATH = 'path';
+    const SIZE = 'size';
+    const MIMETYPE = 'mimetype';
+    const USE_YN = 'use_yn';
+    const CREATED_AT = 'created_at';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'file_seq', 'logical_name', 'physical_name', 'path', 'size', 'mimetype', 'use_yn', 'created_at'
-    ];    
+        self::FILE_SEQ,
+        self::LOGICAL_NAME,
+        self::PHYSICAL_NAME,
+        self::PATH,
+        self::SIZE,
+        self::MIMETYPE,
+        self::USE_YN,
+        self::CREATED_AT
+    ];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,23 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
+/**
+ * @property int $user_seq
+ * @property string $id
+ * @property string $name
+ * @property string $nickname
+ * @property string $password
+ * @property string $email
+ * @property string $tel
+ * @property string $comment
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property int $profile_file_seq
+ * 
+ * @property Collection|File[] $file
+ * 
+ * @package App\Models
+ */
 class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
@@ -15,23 +32,35 @@ class User extends Authenticatable implements JWTSubject
     protected $table = 'tb_user';
     protected $primaryKey = 'user_seq';
 
+    const USER_SEQ = 'user_seq';
+    const ID = 'id';
+    const NAME = 'name';
+    const NICKNAME = 'nickname';
+    const PASSWORD = 'password';
+    const EMAIL = 'email';
+    const TEL = 'tel';
+    const COMMENT = 'comment';
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+    const PROFILE_FILE_SEQ = 'profile_file_seq';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'user_seq', 
-        'id', 
-        'name', 
-        'nickname', 
-        'password', 
-        'email', 
-        'tel', 
-        'comment', 
-        'created_at', 
-        'updated_at'
-    ];    
+        self::ID,
+        self::NAME,
+        self::NICKNAME,
+        self::PASSWORD,
+        self::EMAIL,
+        self::TEL,
+        self::COMMENT,
+        self::CREATED_AT,
+        self::UPDATED_AT,
+        self::PROFILE_FILE_SEQ
+    ];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -39,7 +68,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password'
+        self::PASSWORD
     ];
 
     /**
@@ -60,6 +89,10 @@ class User extends Authenticatable implements JWTSubject
             'id' => $this->id,
             'email' => $this->email,
         ];
+    }
+
+    public function file() {
+        return this->hasMany(File::class, FILE::FILE_SEQ, self::PROFILE_FILE_SEQ);
     }
 
 }
